fd85fec3628fb098acd186c46d2630692ab98a23
nezu <mel@nezu.world>
2023-08-16 07:50:45 -0400

    blank slate for sanity setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,20 +2,20 @@ import "./globals.css"
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
-  title: {
-    default: "nezu.world",
-    template: "nezu.world | %s",
-  }
+	title: {
+		default: "nezu.world",
+		template: "nezu.world | %s",
+	}
 }
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        <main>
-          {children}
-        </main>
-      </body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<body>
+				<main>
+					{children}
+				</main>
+			</body>
+		</html>
+	)
 }
